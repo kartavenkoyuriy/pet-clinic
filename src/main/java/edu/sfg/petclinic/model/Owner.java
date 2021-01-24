54fbdb902f8fa204421ce0a97a243a089240cb61
Yuriy Kartavenko <kartavenko.ya@gmail.com>
2021-01-24 06:49:55 +0200

    Added POJOs. Closes #2.

---
+++ src/main/java/edu/sfg/petclinic/model/Owner.java
@@ -0,0 +1,4 @@
+package edu.sfg.petclinic.model;
+
+public class Owner extends Person {
+}
---
+++ src/main/java/edu/sfg/petclinic/model/PetType.java
@@ -0,0 +1,13 @@
+package edu.sfg.petclinic.model;
+
+public class PetType {
+    private String petType;
+
+    public String getPetType() {
+        return petType;
+    }
+
+    public void setPetType(String petType) {
+        this.petType = petType;
+    }
+}
