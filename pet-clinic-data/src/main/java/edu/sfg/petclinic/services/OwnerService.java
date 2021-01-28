2570ee4b8721f294b0883477d818f620a0638ccb
Yuriy Kartavenko <kartavenko.ya@gmail.com>
2021-01-28 08:02:13 +0200

    added interfaces

---
+++ pet-clinic-data/src/main/java/edu/sfg/petclinic/services/OwnerService.java
@@ -0,0 +1,12 @@
+package edu.sfg.petclinic.services;
+
+import edu.sfg.petclinic.model.Owner;
+
+import java.util.Set;
+
+public interface OwnerService {
+    Owner findByLastName(String lastName);
+    Owner findById(Long id);
+    Owner save(Owner owner);
+    Set<Owner> findAll();
+}
---
+++ pet-clinic-data/src/main/java/edu/sfg/petclinic/services/VetService.java
@@ -0,0 +1,11 @@
+package edu.sfg.petclinic.services;
+
+import edu.sfg.petclinic.model.Vet;
+
+import java.util.Set;
+
+public interface VetService {
+    Vet findById(Long id);
+    Vet save(Vet vet);
+    Set<Vet> findAll();
+}
