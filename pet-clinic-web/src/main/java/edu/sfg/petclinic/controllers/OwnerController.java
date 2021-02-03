f18b7a86faf206e6f6103b363a90b27ea2990755
Yuriy Kartavenko <kartavenko.ya@gmail.com>
2021-02-03 06:38:32 +0200

    added controller and index page for Vet, Owner. closes #7, closes #13

---
+++ pet-clinic-web/src/main/java/edu/sfg/petclinic/controllers/OwnerController.java
@@ -0,0 +1,17 @@
+package edu.sfg.petclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/owners")
+public class OwnerController {
+
+    @RequestMapping({"", "/", "index", "index.html"})
+    public String getAllOwners(){
+
+
+        return "owners/index";
+    }
+
+}
---
+++ pet-clinic-web/src/main/java/edu/sfg/petclinic/controllers/VetController.java
@@ -0,0 +1,18 @@
+package edu.sfg.petclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/vets")
+public class VetController {
+
+    @RequestMapping({"", "/", "index", "index.html"})
+    public String getAllVets(){
+
+
+
+        return "vets/index";
+    }
+
+}
